c9256a1f07d24cb2e9828baa24e12625f45d35d1
Luis F. Tello A. <seizin00@tuta.io>
2026-08-24 17:14:45 -0500

    perdón, el orden importa

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,12 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from scanner.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# Inicializa la aplicación ASGI de Django
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from scanner.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
