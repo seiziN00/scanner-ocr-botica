8c237f257535d69f37efb5a3839da5eb87392a82
Luis F. Tello A. <seizin00@tuta.io>
2026-08-24 16:12:44 -0500

    fix: inicializar apps primero

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,16 @@
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from scanner.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
 
     "websocket": URLRouter(
         websocket_urlpatterns
