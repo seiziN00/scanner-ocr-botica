135bb5c157d87925aa8ffc46a99fead1ce4c50fe
Luis F. Tello A. <seizin00@tuta.io>
2026-08-25 00:58:03 -0500

    Usar la zona horaria peruana

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ else:
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 LANGUAGE_CODE = 'es-pe'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 USE_I18N = True
 USE_TZ = True
 
